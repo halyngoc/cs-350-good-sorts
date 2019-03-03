fab133b662e77475156deb5456946dcbd8734f8f
cSchwarz117 <38546318+cSchwarz117@users.noreply.github.com>
2019-03-02 18:02:23 -0800

    Loop for Randoms
    Made the data gather more appropreate

--- dataSet.py
+++ dataSet.py
@@ -4,10 +4,37 @@ from mergeSort import mergeSort
 from mergeSort import printArray
 
 #random sorting
-start = time.clock()
-array = numpy.random.randint(0, 1000000, 10000000)
-total = time.clock() - start
-print("Total seconds to sort", total)
+
+randomTimes = numpy.zeros(100, dtype=float)
+for x in range(0, 99):
+    array = numpy.random.randint(0, 10000, 10000)
+    start = time.clock()
+    mergeSort(array)
+    total = time.clock() - start
+    randomTimes[x] = total
+
+
+
+
+
+printArray(randomTimes)
+
 #printArray(array)
 #printArray(mergeSort(array))
+
+#sorted sorting
+array = numpy.random.randint(0, 1000, 100000)
+array = numpy.sort(array)
+start = time.clock()
+mergeSort(array)
+total = time.clock() - start
+print("Total seconds to sort", total)
+
+
+#reverse order sorting
+start = time.clock()
+#array2 = numpy.random_sample(0, 1000000, 10000000)
+total = time.clock() - start
+
+#compare floats
 
